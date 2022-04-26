72c51be98304eeb1800d94ebb0880e267014d1d7
Gustavo Kunde <gustavokunde@gmail.com>
2022-04-26 20:45:06 -0300

    created started structure

---
+++ frontend/routes/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+const routes: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  );
+};
+
+export default routes;
